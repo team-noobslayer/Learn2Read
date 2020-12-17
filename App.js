31681e87fd5d623d8dca3143ec40b710dd15f6ff
Richard Coucoules <coules93@gmail.com>
2020-12-17 00:28:32 -0800

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  createAppContainer,
+  // createSwitchNavigator
+} from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+// import {createTabNavigator} from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import HomeScreen from "./src/screens/HomeScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const navigator = createStackNavigator(
+  { Home: HomeScreen },
+  { initialRouteName: "Home" }
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text h1>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
