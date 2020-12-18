00822a20722707f737832ce607828d244d757543
Richard Coucoules <coules93@gmail.com>
2020-12-17 17:00:07 -0800

    Linting

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import {
-  createAppContainer
+  createAppContainer,
   // createSwitchNavigator
 } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 // import {createTabNavigator} from 'react-navigation-tabs';
 
 import HomeScreen from "./src/screens/HomeScreen";
-import ProfileScreen from './src/screens/ProfileScreen'
-import RecordScreen from './src/screens/RecordScreen'
-import QuizScreen from './src/screens/QuizScreen'
+import ProfileScreen from "./src/screens/ProfileScreen";
+import RecordScreen from "./src/screens/RecordScreen";
+import QuizScreen from "./src/screens/QuizScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Record: RecordScreen,
     Profile: ProfileScreen,
-    Quiz: QuizScreen
-
+    Quiz: QuizScreen,
   },
-  { initialRouteName: "Home",
+  {
+    initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: 'App'
-    }
+      title: "App",
+    },
   }
 );
 
--- src/screens/QuizScreen.js
+++ src/screens/QuizScreen.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const QuizScreen = () => {
   return (
     <View>
       <Text> Sample Text:</Text>
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
-export default QuizScreen
+export default QuizScreen;
