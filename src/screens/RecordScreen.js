ada8ed1965285da40150402c0b38ad18457b81ff
Richard Coucoules <coules93@gmail.com>
2020-12-20 14:27:38 -0800

    Add Flatlist child component

--- src/screens/RecordScreen.js
+++ src/screens/RecordScreen.js
@@ -1,31 +1,54 @@
-import React from "react";
-import { View, Text, StyleSheet, Platform } from "react-native";
-import Colors from '../constants/colors';
-import Card from '../components/Card';
+import React, { useState, useContext, useEffect } from "react";
+import { View, Text, StyleSheet, Platform, FlatList } from "react-native";
+
+import { Context as QuizContext } from "../context/quizContext";
+
+import Colors from "../constants/colors";
+import Card from "../components/Card";
 
 const RecordScreen = () => {
+  const { getResponses } = useContext(QuizContext);
+  const [responses, setResponses] = useState([]);
+
+  useEffect(() => {
+    getResponses((responses) => setResponses(responses));
+  }, []);
+
   return (
-    <Card style={styles.titleContainer}>
-      <Text>Records</Text>
-    </Card>
+    <>
+      <Card style={styles.titleContainer}>
+        <Text>Records</Text>
+      </Card>
+      <FlatList
+        data={responses}
+        renderItem={({ item }) => {
+          return (
+            <Text>
+              {item.questionId} {item.response}
+            </Text>
+          );
+        }}
+        keyExtractor={(item) => item.timestamp.getTime().toString()}
+      />
+    </>
   );
 };
 
-RecordScreen.navigationOptions ={
-  headerTitle: 'Records',
+RecordScreen.navigationOptions = {
+  headerTitle: "Records",
   headerStyle: {
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white'
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
   },
-  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
 };
 
 const styles = StyleSheet.create({
   titleContainer: {
     width: 500,
-    maxWidth: '90%',
-   alignItems: 'center',
-   justifyContent: 'center'
-  }
+    maxWidth: "90%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default RecordScreen;
