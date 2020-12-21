36d1dab3e457b8e1fa569ead0b62f67dfdf03e49
Richard Coucoules <coules93@gmail.com>
2020-12-20 16:02:31 -0800

    Update RecordScreen

--- src/screens/RecordScreen.js
+++ src/screens/RecordScreen.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext, useEffect } from "react";
 import { View, Text, StyleSheet, Platform, FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
 
+import useDatabase from "../hooks/useDatabase";
 import { Context as QuizContext } from "../context/quizContext";
 
 import Colors from "../constants/colors";
@@ -9,6 +11,7 @@ import Card from "../components/Card";
 const RecordScreen = () => {
   const { getResponses } = useContext(QuizContext);
   const [responses, setResponses] = useState([]);
+  const [_, getQuestionById] = useDatabase();
 
   useEffect(() => {
     getResponses((responses) => setResponses(responses));
@@ -22,13 +25,23 @@ const RecordScreen = () => {
       <FlatList
         data={responses}
         renderItem={({ item }) => {
+          getQuestionById(
+            item.questionId,
+            (question) => (item.question = question)
+          );
           return (
-            <Text>
-              {item.questionId}
-              {item.response}
-              {item.correct ? "✔️" : "❌"}
-              {item.timestamp.toString()}
-            </Text>
+            <View>
+              <ListItem bottomDivider>
+                <ListItem.Content>
+                  <ListItem.Title>
+                    Question ID: {item.questionId}
+                  </ListItem.Title>
+                  <ListItem.Subtitle>
+                    {item.response} {item.correct ? "✔️" : "❌"}
+                  </ListItem.Subtitle>
+                </ListItem.Content>
+              </ListItem>
+            </View>
           );
         }}
         keyExtractor={(item) => item.timestamp.getTime().toString()}
